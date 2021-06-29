40a2de3fadc6b4efbc51b4d3a9151d5e55bb92ac
Truth <henrysempire111gmail.com>
2021-06-29 12:15:30 -0700

    updated settings

--- Query/settings.py
+++ Query/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-^n_kwah7nfk)9o5j26@^+p2_(vdx&dr39vy#mumw=@ty=-hsp8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['querybackendapi.herokuapp.com']
 
 
 # Application definition
